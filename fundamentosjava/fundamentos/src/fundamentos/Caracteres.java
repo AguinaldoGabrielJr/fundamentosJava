b3da6526b494cab5dfe08298b42335e5834d4179
Aguinaldo Gabriel dos Santos Junior <guina.santos.cwb@gmail.com>
2022-08-22 11:27:30 -0300

    Strings

---
+++ fundamentosjava/fundamentos/src/fundamentos/Caracteres.java
@@ -0,0 +1,14 @@
+package fundamentos;
+
+public class Caracteres {
+
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
---
+++ fundamentosjava/fundamentos/src/fundamentos/Main.java
@@ -0,0 +1,12 @@
+package fundamentos;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Caracteres c1 = new Caracteres();
+		c1.setNome("Guina");
+		System.out.println(c1.getNome());
+	}
+
+}
